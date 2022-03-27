33ab83ecef2c86c425bb682a9c5b3ddbb06ec53e
alan.s.paul09@gmail.com <alan.s.paul09@gmail.com>
2022-03-27 17:29:09 +0200

    Remove unnecessary comments

--- Project1/task4.py
+++ Project1/task4.py
@@ -26,11 +26,8 @@ def calib_imu2velo():
     return imu_to_velo
 
 def main():
-    # index = "0000000037"
-    # index = "0000000043"
-    # index = "0000000320"
+    index = "0000000037"
     # index = "0000000077"
-    index = "0000000310"
 
     calib_velo2cam_mat = calib_velo2cam("./data/problem_4/calib_velo_to_cam.txt")
     R, T = calib_velo2cam_mat
@@ -40,37 +37,33 @@ def main():
     world_points = load_from_bin(f"./data/problem_4/velodyne_points/data/{index}.bin")
     distances = np.linalg.norm(world_points, axis=1)
     depth_colors = depth_color(distances)
+
     # Visualize distorted point cloud
     filtered_projected_points, [filtered_depth_colors, filtered_z_cameras] = compute_projected_points(world_points, K, extrinsic, image.shape, [depth_colors])
     image_with_colors = print_projection_plt(filtered_projected_points.T, filtered_depth_colors, image) 
     plt.imshow(image_with_colors)
+    plt.axis("off")
     plt.show()
 
     velodyne_timestamp_start = compute_timestamps("./data/problem_4/velodyne_points/timestamps_start.txt", index)
     velodyne_timestamp_end = compute_timestamps("./data/problem_4/velodyne_points/timestamps_end.txt", index)
-    # We can use either image_timestamps or velodyne trigger timestamps.
-    # image_timestamp = compute_timestamps("./data/problem_4/image_02/timestamps.txt", index)
     image_timestamp = compute_timestamps("./data/problem_4/velodyne_points/timestamps.txt", index)
     oxts_timestamp = compute_timestamps("./data/problem_4/oxts/timestamps.txt", index)
     oxts_velocity = load_oxts_velocity(f"./data/problem_4/oxts/data/{index}.txt")
     oxts_angular_rate = load_oxts_angular_rate(f"./data/problem_4/oxts/data/{index}.txt")
 
-    '''
-    - We assume that each scan takes exactly 1/10th of a second. 
-    - We calculate the time difference between image trigger and each 3d point's laser.
-    - And then use this time to find the amount of distortion (by multiplying with the velocity).
-    '''
+    scan_time = velodyne_timestamp_end - velodyne_timestamp_start
 
     # Compute angles between 3D points and forward-facing axis
     world_point_angles = np.arctan2(world_points[:, 1], world_points[:, 0]) * (180/np.pi) # swapped x and y coords because of np.arctan2 requirements
-    start_to_image_angle = ((image_timestamp - velodyne_timestamp_start) / 0.1) * 360
+    start_to_image_angle = ((image_timestamp - velodyne_timestamp_start) / scan_time) * 360
 
     # The time of laser is the time when the laser was fired
-    time_of_laser = velodyne_timestamp_start + 0.1 * (( (start_to_image_angle + world_point_angles) % 360) / 360.0)
+    time_of_laser = velodyne_timestamp_start + scan_time * (( (start_to_image_angle + world_point_angles) % 360) / 360.0)
     image_to_laser = time_of_laser - image_timestamp # negative implies laser fired before image triggered
 
     # Convert to imu coords
-    homogeneous_world_points = np.hstack((world_points, np.ones(world_points.shape[0]).reshape(-1, 1))) #Add a column of 1 to have homogeneous points
+    homogeneous_world_points = np.hstack((world_points, np.ones(world_points.shape[0]).reshape(-1, 1))) # Add a column of 1 to have homogeneous points
     imu_to_velo = calib_imu2velo()
     velo_to_imu = np.linalg.inv(imu_to_velo)
     imu_world_points = (velo_to_imu@homogeneous_world_points.T).T
@@ -96,7 +89,9 @@ def main():
     filtered_projected_points, [filtered_depth_colors, filtered_z_cameras] = compute_projected_points(corrected_velo_world_points[:, :3], K, extrinsic, image.shape, [depth_colors])
     corrected_image_with_colors = print_projection_plt(filtered_projected_points.T, filtered_depth_colors, image) 
     plt.imshow(corrected_image_with_colors)
+    plt.axis("off")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
