020a27c69d01184aa6c6f5b7169d893c75ed37a2
Loïc Houmard <l456@houmard.com>
2022-05-09 00:20:32 +0200

    Reimplement normals without for loops

--- Project2/mtl/utils/otherTasks.py
+++ Project2/mtl/utils/otherTasks.py
@@ -42,27 +42,34 @@ def compute_normals(depth_image):
         The normal (3D) at each pixel for the whole batch
     """
     B, H, W = depth_image.shape
-
-    output = torch.zeros(B, 3, H, W)
-    for y in range(1, H):
-        for x in range(1, W):
-            """
-            /* * * * *
-             * * t * *
-             * l c * *
-             * * * * */
-            """
-            t = torch.concat([torch.tensor([x, y-1]).repeat(B, 1), depth_image[:, y-1, x].unsqueeze(0).T], dim=1)
-            l = torch.concat([torch.tensor([x-1, y]).repeat(B, 1), depth_image[:, y, x-1].unsqueeze(0).T], dim=1)
-            c = torch.concat([torch.tensor([x, y]).repeat(B, 1), depth_image[:, y, x].unsqueeze(0).T], dim=1)
-            d = torch.cross(torch.sub(l, c), torch.sub(t, c), dim=1)
-            
-            n = torch.nn.functional.normalize(d, dim=1)
-
-            output[:, :, y, x] = n
     
-    # Put same normal in border of the images
+    output = torch.zeros(B, 3, H, W)
+
+    xvalues = torch.arange(0, W)
+    yvalues = torch.arange(0, H)
+
+    """
+    /* * * * *
+    * * t * *
+    * l c * *
+    * * * * */
+    """
+    
+    tyy, txx = torch.meshgrid(yvalues[:H-1], xvalues[1:])
+    t = torch.concat([torch.stack([txx, tyy], dim=2).unsqueeze(0).repeat(B, 1, 1, 1), depth_image[:, :H-1, 1:].unsqueeze(3)], dim=3)
+
+    lyy, lxx = torch.meshgrid(yvalues[1:], xvalues[:W-1])
+    l = torch.concat([torch.stack([lxx, lyy], dim=2).unsqueeze(0).repeat(B, 1, 1, 1), depth_image[:, 1:, :W-1].unsqueeze(3)], dim=3) 
+    
+    cyy, cxx = torch.meshgrid(yvalues[1:], xvalues[1:])
+    c = torch.concat([torch.stack([cxx, cyy], dim=2).unsqueeze(0).repeat(B, 1, 1, 1), depth_image[:, 1:, 1:].unsqueeze(3)], dim=3)
+
+    
+    d = torch.cross(torch.sub(l, c), torch.sub(t, c), dim=3)
+    n = torch.nn.functional.normalize(d, dim=3).transpose(2,3).transpose(1,2)
+    
+    output[:, :, 1:, 1:] = n
     output[:, :, 0, :] = output[:, :, 1, :] 
     output[:, :, :, 0] = output[:, :, :, 1]
-
+    
     return output
