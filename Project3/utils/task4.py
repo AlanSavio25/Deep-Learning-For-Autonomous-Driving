c70940b445842248ba3b8e43d1f5b5eea22e8145
Loïc Houmard <l456@houmard.com>
2022-06-24 11:41:58 +0200

    Implement and test task4

--- Project3/utils/task4.py
+++ Project3/utils/task4.py
@@ -22,7 +22,15 @@ class RegressionLoss(nn.Module):
         useful config hyperparameters
             self.config['positive_reg_lb'] lower bound for positive samples
         '''
-        pass
+        positive_samples = iou >= self.config['positive_reg_lb'] # boolean array (N, )
+        positive_pred = pred[positive_samples]
+        positive_target = target[positive_samples]
+        
+        loss_location = self.loss(positive_pred[:, :3], positive_target[:, :3])
+        loss_size = self.loss(positive_pred[:, 3:6], positive_target[:, 3:6])
+        loss_rotation = self.loss(positive_pred[:, 6], positive_target[:, 6])
+        
+        return loss_location + 3*loss_size + loss_rotation
 
 class ClassificationLoss(nn.Module):
     def __init__(self, config):
@@ -38,10 +46,17 @@ class ClassificationLoss(nn.Module):
         avoid incorrect training signals to supervise our network.  A proposal
         is considered as positive (class 1) if its maximum IoU with ground
         truth boxes is ≥ 0.6, and negative (class 0) if its maximum IoU ≤ 0.45.
-            pred (N,7) predicted bounding boxes
+            pred (N,1) confidence prediction
             iou (N,) initial IoU of all paired proposal-targets
         useful config hyperparameters
             self.config['positive_cls_lb'] lower bound for positive samples
             self.config['negative_cls_ub'] upper bound for negative samples
         '''
-        pass
+        positive_samples = iou >= self.config['positive_cls_lb'] # boolean array (N, )
+        negative_samples = iou <= self.config['negative_cls_ub'] # boolean array (N, )
+        positive_pred = pred[positive_samples]
+        neagtive_pred = pred[negative_samples]
+        final_iou = torch.cat([neagtive_pred, positive_pred], dim=0)
+        final_gt = torch.cat([torch.zeros_like(neagtive_pred), torch.ones_like(positive_pred)])
+
+        return self.loss(final_iou, final_gt)
